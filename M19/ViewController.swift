905551447a574163341494da6bf0352051fde19f
Dudareva <dudareva.yana@outlook.com>
2022-06-25 19:42:16 +0300

    Initial Commit

---
+++ M19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M19
+//
+//  Created by Сергей Щукин on 25.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
